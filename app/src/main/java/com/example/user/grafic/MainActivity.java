3e996bb39b9765ed702067e837741c40bf8a7b19
Users <lana.chebotareva.95@mail.ru>
2018-09-11 16:38:08 +0300

    Add Toast

--- app/src/main/java/com/example/user/grafic/MainActivity.java
+++ app/src/main/java/com/example/user/grafic/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,65 +26,49 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.OnDataPointTapListener;
 import com.jjoe64.graphview.series.Series;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-//    LineGraphSeries lineGraphSeries;
-    private SensorManager mSensorManger;
-    Sensor sensor;
-    GraphView graph;
-    private double graph2LastXValue = 5d;
-    private double graph2LastYValue = 5d;
-    private double graph2LastZValue = 5d;
-    private Double[] dataPoints;
-    LineGraphSeries<DataPoint> series;
-    LineGraphSeries<DataPoint> seriesX;
-    LineGraphSeries<DataPoint> seriesZ;
-    LineGraphSeries<DataPoint> seriesXX;
-    LineGraphSeries<DataPoint> seriesYY;
-    LineGraphSeries<DataPoint> seriesZZ;
-    private Thread thread;
-    private boolean plotData = true;
-    float xx;
-    float yy;
-    float zz;
+public class MainActivity extends AppCompatActivity
 
-    private float On_1 = 1;
-    private float altha = 0.1f;
-Spinner spinner;
+{
+
+
+ Spinner spinner;
     String [] acxios={"Ускорение по оси Х","Ускорение по оси Y", "Ускорение по оси Z "};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner=  findViewById(R.id.spinner);
-        mSensorManger=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+         spinner=  findViewById(R.id.spinner);
+
+
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, acxios);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
         AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
-
-
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String item = (String)parent.getItemAtPosition(position);
 //                System.out.println(position +"!!!!!!!!!!!!!!!!");
 //                System.out.println(id);
                 switch ((int) id){
-                    case 0: System.out.println("x");
-                        Toast toast = Toast.makeText(getApplicationContext(),
-                                "Ускорение по оси ОХ!", Toast.LENGTH_SHORT);
-                        toast.show();
-                        break;
+                    case 0:
+                System.out.println("x");
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Ускорение по оси ОХ!", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
                     case 1: System.out.println("y");
-                        Toast toasts = Toast.makeText(getApplicationContext(),
-                                "Ускорение по оси ОY!", Toast.LENGTH_SHORT);
-                        toasts.show();
-                        break;
-                    case 2: System.out.println("z");
-                        Toast toastss = Toast.makeText(getApplicationContext(),
-                                "Ускорение по оси ОZ!", Toast.LENGTH_SHORT);
-                        toastss.show();
-                        break;
+                Toast toasts = Toast.makeText(getApplicationContext(),
+                        "Ускорение по оси ОY!", Toast.LENGTH_SHORT);
+                toasts.show();
+                break;
+                    case 2:  System.out.println("z");
+                Toast toastss = Toast.makeText(getApplicationContext(),
+                        "Ускорение по оси ОZ!", Toast.LENGTH_SHORT);
+                toastss.show();
+                break;
                 }
             }
 
@@ -94,4 +80,3 @@ Spinner spinner;
         spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 }
-
